Fix UVI units

syotools/models/source_exposure.py
@@ -420,7 +420,8 @@ class SourceSpectrographicExposure(SourceExposure):
 
         iflux = np.interp(wave, swave, sflux, left=0., right=0.)
         self._interp_flux = iflux
-        phot_energy = const.h.to(u.erg * u.s) * const.c.to(u.cm / u.s) / wave.to(u.cm) / u.ct
+        phot_energy = const.h.to(u.erg * u.s) * const.c.to(u.cm / u.s) / wave.to(u.cm) / u.pix
+
         #print('_update_snr phot_energy: ', phot_energy)
 
         scaled_aeff = aeff * (aper / (15 * u.m))**2
@@ -490,7 +491,7 @@ class SourceSpectrographicExposure(SourceExposure):
         if self.verbose:
             print("Exptime: {}".format(t_exp))
 
-        self._exptime = (t_exp.value)*u.s
+        self._exptime = t_exp
 
         return True #completed successfully
 