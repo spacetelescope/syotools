e1628610ecdfbe22b8033c2ad0e6caa57cc408e6
Adric Riedel <adric.riedel@gmail.com>
2026-04-09 12:59:21 -0400

    single-test printing mode

--- tests/test_syotools.py
+++ tests/test_syotools.py
@@ -24,12 +24,21 @@ for item in test_list:
         test_names.append(os.path.split(temp_file)[-1].replace(".yml", ""))
         test_setups.append(single_input)
 
+def formatted_print(expected, result):
+    """
+    Formatting for the single-test runner version
+    """
+    print(f"                 Old:                 New: ")
+    for idx, item in enumerate(expected):
+        print(idx)
+        for value in item:
+            print(f" {value:>6}: {item[value]:>16}           {result[idx][value]:>16}")
+
 @pytest.mark.parametrize("inputs", test_setups, ids = test_names)
 def test_files(inputs):
     testfile = read_yaml(inputs["filename"])
 
-    # if this fails, it gets picked up as an error by Pytest anyway
-
+    # write mode is the manual mode where you're trying to set (just tests without expected values) or reset (all tests)
     write = False
     if ("reset" in inputs and inputs["reset"]) or ("set" in inputs and inputs["set"]):
         write = True
@@ -62,6 +71,9 @@ def test_files(inputs):
     # 2. reset is in the input command AND it's true
     if write == False:
         if "expected" in testfile:
+            # print mode is if you're manually running this code just to get the results of a single test.
+            if "print" in inputs:
+                formatted_print(testfile["expected"], result)
             assert check_relative_diff(result, testfile["expected"], 0.0005) #1e-3)
         else:
             assert False, f"No comparison in file {inputs['filename']}."
@@ -71,7 +83,12 @@ def test_files(inputs):
             write_yaml(testfile, inputs["filename"])
 
 if __name__ == "__main__":
-    for test in test_setups:
-        if len(sys.argv) > 0:
-            test[sys.argv[1]] = True
-        test_files(test)
+    if len(sys.argv) > 0:
+        additional = sys.argv[1]
+        if additional in ["set", "reset"]:
+            for test in test_setups:
+                test[sys.argv[1]] = True
+                test_files(test)
+        else:
+            test_names = [additional]
+            test_files({"filename": additional, "print": True})
